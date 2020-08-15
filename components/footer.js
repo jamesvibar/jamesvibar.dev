00978896c924e7071510cee52a575a4a1d9841b2
James Raphael Vibar <vibarjamess@gmail.com>
2020-08-15 09:12:07 +0800

    feat(homepage): add social links

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,12 @@
 /** @jsx jsx */
 import React from 'react'
 import { jsx } from 'theme-ui'
+import {
+  AiOutlineTwitter,
+  AiOutlineGithub,
+  AiFillLinkedin,
+} from 'react-icons/ai'
+
 import Container from '../elements/container'
 import Wrapper from '../elements/wrapper'
 
@@ -18,10 +24,50 @@ export default function Footer() {
       <Container>
         <Wrapper>
           <div sx={{ height: '75px', display: 'flex', alignItems: 'center' }}>
-            &copy; {year}
+            <span>
+              &copy; {year}
+            </span>
+            <div sx={{ ml: 'auto' }}>
+              <SocialLink
+                href="https://twitter.com/jemsvibar"
+                rel="nofollow"
+                target="_blank"
+              >
+                <AiOutlineTwitter />
+              </SocialLink>
+              <SocialLink
+                href="https://github.com/jamesvibar"
+                rel="nofollow"
+                target="_blank"
+              >
+                <AiOutlineGithub />
+              </SocialLink>
+              <SocialLink
+                href="https://www.linkedin.com/in/james-vibar"
+                rel="nofollow"
+                target="_blank"
+              >
+                <AiFillLinkedin />
+              </SocialLink>
+            </div>
           </div>
         </Wrapper>
       </Container>
     </footer>
+  )
+}
+
+const SocialLink = ({ children, ...props }) => {
+  return (
+    <a
+      sx={{
+        color: 'white',
+        p: '4px',
+        '&:hover': { color: '#1FCDC3' },
+      }}
+      {...props}
+    >
+      {children}
+    </a>
   )
 }
